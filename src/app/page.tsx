c518b048506f38b9a868d97c0bd2c9af126933f0
Павел Урядышев <pauryadyshev@sberbank.ru>
2023-11-17 16:07:11 +0300

    init styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Typography, Container, Card, Box } from '@mui/material'
+import { Typography, Container, Card, Box, Button } from '@mui/material'
 
 export default function Home() {
   return (
@@ -6,12 +6,82 @@ export default function Home() {
       <Container>
         <Box>
           <Card>
-            <Typography variant="h2">Lest&apos;s get started</Typography>
-            <Box sx={{ height: 50, width: 50 }} bgcolor="primary.main" />
-            <Box sx={{ height: 50, width: 50 }} bgcolor="secondary.main" />
-            <Box sx={{ height: 50, width: 50 }} bgcolor="error.main" />
-            <Box sx={{ height: 50, width: 50 }} bgcolor="warning.main" />
-            <Box sx={{ height: 50, width: 50 }} bgcolor="success.main" />
+            <Box display="flex">
+              <Box>
+                <Box sx={{ height: 50, width: 50 }} bgcolor="primary.main" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="secondary.main" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="error.main" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="warning.main" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="success.main" />
+              </Box>
+              <Box style={{ marginLeft: 16 }}>
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.700" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.600" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.500" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.400" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.300" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.200" />
+                <Box sx={{ height: 50, width: 50 }} bgcolor="grey.100" />
+              </Box>
+            </Box>
+          </Card>
+          <Card>
+            <Box display="flex">
+              <Box>
+                <Typography variant="h1">Heading 1</Typography>
+                <Typography variant="h2">Heading 2</Typography>
+                <Typography variant="h3">Heading 3</Typography>
+                <Typography variant="h4">Heading 4</Typography>
+                <Typography variant="h5">Heading 5</Typography>
+                <Typography variant="h6">Heading 6</Typography>
+                <Typography variant="h7">Heading 7</Typography>
+              </Box>
+              <Box style={{ marginLeft: 16 }}>
+                <Typography variant="text1">Text 26</Typography>
+                <Typography variant="text2">Text 22</Typography>
+                <Typography variant="body1">Text 20</Typography>
+                <Typography variant="text3">Text 18</Typography>
+                <Typography variant="body2">Text 16</Typography>
+                <Typography variant="caption1">Text 14</Typography>
+                <br />
+                <Typography variant="caption2">Text 12</Typography>
+              </Box>
+              <Box style={{ marginLeft: 16 }}>
+                <Typography variant="text1Semi">Text 26</Typography>
+                <Typography variant="text2Semi">Text 22</Typography>
+                <Typography variant="body1Semi">Text 20</Typography>
+                <Typography variant="text3Semi">Text 18</Typography>
+                <Typography variant="body2Semi">Text 16</Typography>
+                <Typography variant="caption1Semi">Text 14</Typography>
+                <br />
+                <Typography variant="caption2Semi">Text 12</Typography>
+              </Box>
+              <Box style={{ marginLeft: 16 }}>
+                <Typography variant="text1Bold">Text 26</Typography>
+                <Typography variant="text2Bold">Text 22</Typography>
+                <Typography variant="body1Bold">Text 20</Typography>
+                <Typography variant="text3Bold">Text 18</Typography>
+                <Typography variant="body2Bold">Text 16</Typography>
+                <Typography variant="caption1Bold">Text 14</Typography>
+                <br />
+                <Typography variant="caption2Bold">Text 12</Typography>
+              </Box>
+              <Box style={{ marginLeft: 16 }}>
+                <Typography variant="subtitle1">Subtitle 1</Typography>
+                <Typography variant="subtitle2">Subtitle 2</Typography>
+              </Box>
+            </Box>
+          </Card>
+          <Card>
+            <Box mt={5} mb={5}>
+              <Button variant="contained">Button contained</Button>
+              <Button variant="outlined" color="secondary">
+                Button outlined
+              </Button>
+              <Button variant="text" color="primary" size="small">
+                Button text
+              </Button>
+            </Box>
           </Card>
         </Box>
       </Container>
--- src/providers/ThemeRegistry/theme.ts
+++ src/providers/ThemeRegistry/theme.ts
@@ -1,4 +1,25 @@
 import { createTheme } from '@mui/material'
+import localFont from 'next/font/local'
+
+const poppinsFont = localFont({
+  src: './assets/fonts/poppins/Poppins-Medium.ttf',
+})
+
+const interFontReguar = localFont({
+  src: './assets/fonts/inter/static/Inter-Regular.ttf',
+})
+
+const interFontMedium = localFont({
+  src: './assets/fonts/inter/static/Inter-Medium.ttf',
+})
+
+const interFontSemi = localFont({
+  src: './assets/fonts/inter/static/Inter-SemiBold.ttf',
+})
+
+const interFontBold = localFont({
+  src: './assets/fonts/inter/static/Inter-Bold.ttf',
+})
 
 export const theme = createTheme({
   palette: {
@@ -17,5 +38,332 @@ export const theme = createTheme({
     success: {
       main: '#38CB89',
     },
+    grey: {
+      '700': '#141718',
+      '600': '#232627',
+      '500': '#343839',
+      '400': '#6C7275',
+      '300': '#E8ECEF',
+      '200': '#F3F5F7',
+      '100': '#FEFEFE',
+    },
   },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          /**
+           * Headings
+           */
+          h7: 'h6',
+          /**
+           * Text
+           */
+          text1: 'p',
+          text1Semi: 'p',
+          text1Bold: 'p',
+          text2: 'p',
+          text2Semi: 'p',
+          text2Bold: 'p',
+          text3: 'p',
+          text3Semi: 'p',
+          text3Bold: 'p',
+          body1Semi: 'p',
+          body1Bold: 'p',
+          body2Semi: 'p',
+          body2Bold: 'p',
+          caption1: 'span',
+          caption1Semi: 'span',
+          caption1Bold: 'span',
+          caption2: 'span',
+          caption2Semi: 'span',
+          caption2Bold: 'span',
+        },
+      },
+      variants: [
+        {
+          props: { variant: 'text1Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.text1,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'text1Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.text1,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'text2Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.text2,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'text2Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.text2,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'text3Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.text3,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'text3Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.text3,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'body1Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.body1,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'body1Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.body1,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'body2Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.body2,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'body2Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.body2,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'caption1Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.caption1,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'caption1Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.caption1,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'caption2Semi' },
+          style: ({ theme }) => ({
+            ...theme.typography.caption2,
+            fontWeight: 600,
+            fontFamily: interFontSemi.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'caption2Bold' },
+          style: ({ theme }) => ({
+            ...theme.typography.caption2,
+            fontWeight: 700,
+            fontFamily: interFontBold.style.fontFamily,
+          }),
+        },
+        {
+          props: { variant: 'subtitle1' },
+          style: ({ theme }) => ({
+            ...theme.typography.body1,
+          }),
+        },
+        {
+          props: { variant: 'subtitle2' },
+          style: ({ theme }) => ({
+            ...theme.typography.text3,
+          }),
+        },
+      ],
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          textTransform: 'none',
+          borderRadius: 8,
+          padding: `${ownerState.size === 'small' ? 5 : 10}px 40px`,
+          fontSize: '16px',
+          fontStyle: 'normal',
+          fontWeight: 500,
+          lineHeight: '28px',
+          letterSpacing: '-0.4px',
+          fontFamily: interFontMedium.style.fontFamily,
+          height: ownerState.size === 'small' ? 28 : 40,
+        }),
+        textPrimary: ({ theme }) => ({
+          color: theme.palette.grey['700'],
+          borderBottomColor: `1px solid ${theme.palette.grey['700']}`,
+        }),
+        text: {
+          paddingLeft: 0,
+          paddingRight: 0,
+          borderBottomWidth: '1px',
+          borderBottomStyle: 'solid',
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+          '&:hover': {
+            background: 'transparent',
+          },
+        },
+        outlinedSecondary: ({ theme }) => ({
+          border: 'none',
+          color: theme.palette.grey['400'],
+          '&:hover': {
+            border: 'none',
+            background: theme.palette.grey['200'],
+          },
+        }),
+      },
+    },
+  },
+  typography: palette => ({
+    /**
+     * Default
+     */
+    allVariants: {
+      fontFamily: interFontReguar.style.fontFamily,
+      color: palette.grey['700'],
+    },
+    /**
+     * Headings
+     */
+    h1: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '80px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '84px',
+      letterSpacing: '-3px',
+    },
+    h2: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '72px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '76px',
+      letterSpacing: '-2px',
+    },
+    h3: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '54px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '58px',
+      letterSpacing: '-1px',
+    },
+    h4: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '40px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '44px',
+      letterSpacing: '-0.4px',
+    },
+    h5: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '34px',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      lineHeight: '38px',
+      letterSpacing: '-0.6px',
+    },
+    h6: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '28px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '34px',
+      letterSpacing: '-0.6px',
+    },
+    h7: {
+      fontFamily: poppinsFont.style.fontFamily,
+      fontSize: '20px',
+      fontStyle: 'normal',
+      fontWeight: 500,
+      lineHeight: '28px',
+      color: palette.grey['600'],
+    },
+    /**
+     * Text
+     */
+    text1: {
+      fontSize: '26px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '40px',
+    },
+    text2: {
+      fontSize: '22px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '34px',
+    },
+    body1: {
+      fontSize: '20px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '32px',
+    },
+    text3: {
+      fontSize: '18px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '30px',
+    },
+    body2: {
+      fontSize: '16px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '26px',
+    },
+    caption: {
+      fontSize: '14px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '22px',
+    },
+    caption1: {
+      fontSize: '14px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '22px',
+    },
+    caption2: {
+      fontSize: '12px',
+      fontStyle: 'normal',
+      fontWeight: 400,
+      lineHeight: '20px',
+    },
+  }),
 })
