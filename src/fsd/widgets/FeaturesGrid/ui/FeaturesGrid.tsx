8fe8d7aa1fc927d1b860606247535a68cb1b0875
Pavel Uryadyshev <pavel111pavel@yandex.ru>
2025-08-06 12:50:01 +0300

    fix(home): implement loading state for Advantages section

--- src/fsd/widgets/FeaturesGrid/ui/FeaturesGrid.tsx
+++ src/fsd/widgets/FeaturesGrid/ui/FeaturesGrid.tsx
@@ -8,21 +8,32 @@ import {
 import { Box, Grid } from '@mui/material'
 
 export const FeaturesGrid: React.FC = () => {
-  const { data } = useGetAdvantagesQuery()
+  const { data, isLoading } = useGetAdvantagesQuery()
   const { getIconByAdvantageKind } = useAdvantagesUiData()
 
   return (
     <Box sx={{ mt: 1 }}>
       <Grid container spacing={{ sm: 3, xs: 1 }} sx={{ mt: { sm: 0, xs: -2 } }}>
-        {data?.items.map(({ description, id, kind, title }) => (
-          <Grid key={id} size={{ md: 3, xs: 6 }} sx={{ mt: { sm: 0, xs: 2 } }}>
-            <Advantage
-              description={description}
-              icon={getIconByAdvantageKind(kind)}
-              title={title}
-            />
-          </Grid>
-        ))}
+        {isLoading &&
+          Array.from({ length: 4 }).map((_, i) => (
+            <Grid key={i} size={{ md: 3, xs: 6 }} sx={{ mt: { sm: 0, xs: 2 } }}>
+              <Advantage skeleton />
+            </Grid>
+          ))}
+        {!isLoading &&
+          data?.items.map(({ description, id, kind, title }) => (
+            <Grid
+              key={id}
+              size={{ md: 3, xs: 6 }}
+              sx={{ mt: { sm: 0, xs: 2 } }}
+            >
+              <Advantage
+                description={description}
+                icon={getIconByAdvantageKind(kind)}
+                title={title}
+              />
+            </Grid>
+          ))}
       </Grid>
     </Box>
   )
